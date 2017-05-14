6e531f485a1f85144225a37c208611185cee2f97
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2017-05-14 12:13:50 +0300

    Создал рабочую группу

--- backend/cactus/api/urls.py
+++ backend/cactus/api/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     url(r'^tasks/add/$', TaskCreateView.as_view()),
     url(r'^subjects/$', SubjectListView.as_view()),
 
-    url(r'group/tasks/$', GroupTaskListView.as_view()),
-    url(r'group/tasks/add/$', GroupTaskCreateView.as_view()),
+    url(r'^group/tasks/$', GroupTaskListView.as_view()),
+    url(r'^group/tasks/add/$', GroupTaskCreateView.as_view()),
 
-    url(r'subjects/(?P<pk>\d+)/$', SubjectRetrieveView.as_view()),
-#    url(r'subjects/status/$', SubjectRetrieveView),
+    url(r'^subjects/(?P<pk>\d+)/$', SubjectRetrieveView.as_view()),
+    # url(r'^subjects/(?P<pk>\d+)/mark_checked/')
 ]
